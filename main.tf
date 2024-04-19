e580814c18dd54559cb10ca3d9ec9a1cc6b9a1db
m01344 <88671468+m01344@users.noreply.github.com>
2024-04-19 14:36:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
+  secret = "asdas123123"
 
   tags = {
     Name = "rg"
